a83c097ddea8271e3989ad936e683c632b138b35
Nounz27 <mathiasenrique.sanchez@gmail.com>
2026-05-09 13:46:07 -0500

    feat: integrate PrimeVue and Pinia for state management and UI components.

--- src/main.js
+++ src/main.js
@@ -1,5 +1,16 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import PrimeVue from 'primevue/config';
+import Material from '@primeuix/themes/material';
+import 'primeflex/primeflex.css';
+import 'primeicons/primeicons.css';
+import i18n from './i18n.js';
+import pinia from './pinia.js';
 
-createApp(App).mount('#app')
+createApp(App)
+    .use(pinia)
+    .use(i18n)
+    .use(PrimeVue, {ripple: true, theme: {preset: Material}})
+    .mount('#app')
+
