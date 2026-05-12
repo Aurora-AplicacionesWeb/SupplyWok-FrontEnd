6ca5639ba6a2b7427bd6a124d4345edac7ccf1e3
Nounz27 <mathiasenrique.sanchez@gmail.com>
2026-05-11 20:23:13 -0500

    chore: add new SVG icons for alerts, configuration, dashboard, inventory, kitchen tickets, orders, reports, subscription, and suppliers

--- src/main.js
+++ src/main.js
@@ -1,16 +1,25 @@
 import { createApp } from 'vue'
 import './style.css'
-import App from './App.vue'
-import PrimeVue from 'primevue/config';
-import Material from '@primeuix/themes/material';
-import 'primeflex/primeflex.css';
-import 'primeicons/primeicons.css';
-import i18n from './i18n.js';
-import pinia from './pinia.js';
+import App from './app.vue'
+import router from './router.js'
+import pinia from './pinia.js'
+
+import PrimeVue from 'primevue/config'
+import Aura from '@primeuix/themes/aura'
+import 'primeflex/primeflex.css'
+import 'primeicons/primeicons.css'
+
+import { Button, InputText, Card, SelectButton } from 'primevue'
+
+import i18n from './i18n.js'
 
 createApp(App)
-    .use(pinia)
     .use(i18n)
-    .use(PrimeVue, {ripple: true, theme: {preset: Material}})
+    .use(pinia)
+    .use(router)
+    .use(PrimeVue, { theme: { preset: Aura }, ripple: true })
+    .component('pv-button', Button)
+    .component('pv-input-text', InputText)
+    .component('pv-card', Card)
+    .component('pv-select-button', SelectButton)
     .mount('#app')
-
