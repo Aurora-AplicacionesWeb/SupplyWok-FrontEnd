9e705a86ef2bebbb95f9a89f8c2f92045b7e2833
Nounz27 <mathiasenrique.sanchez@gmail.com>
2026-05-11 20:08:27 -0500

    feat(restaurant-management/infrastructure): implement assemblers for restaurant management entities.

---
+++ src/restaurant-management/infrastructure/kitchen-order.assembler.js
@@ -0,0 +1,17 @@
+import {KitchenOrder} from "../domain/model/kitchen-order.entity.js";
+
+export class KitchenOrderAssembler {
+    static toEntityFromResource(resource) {
+        return new KitchenOrder({...resource})
+    }
+
+    static ToEntitiesFromResponse(response) {
+        if (response.status !== 200) {
+            console.error(`${response.status}, ${response.statusText}`);
+            return [];
+        }
+        let resources = response.data instanceof Array ? response.data : response.data['kitchenOrders'];
+
+        return resources.map(resource => this.toEntityFromResource(resource));
+    }
+}
